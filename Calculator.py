9804dc65e34cdf8541e1292a48a14e91fd2afec4
KerollosSamaan75 <kiro.samaan.kodoos@gmail.com>
2024-12-26 00:03:50 +0200

    Calculator

---
+++ Calculator.py
@@ -0,0 +1,10 @@
+first_number = float(input())
+second_number = float(input())
+sum = first_number + second_number
+subtraction = first_number - second_number
+multiplication = first_number * second_number
+division = first_number / second_number if second_number != 0 else "Division by zero is not allowed"
+print(sum)
+print(subtraction)
+print(multiplication)
+print(division)
