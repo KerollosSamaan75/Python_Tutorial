27748d2956420324541f906139d026c7f251fb9e
KerollosSamaan75 <kiro.samaan.kodoos@gmail.com>
2024-12-26 09:31:43 +0200

    Insertion sort

---
+++ InsertionSort.py
@@ -0,0 +1,14 @@
+import random
+
+def insertion_sort():
+    arr = random.sample(range(1, 101), 10)
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    print(arr)
+
+insertion_sort()
